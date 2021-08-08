8b84fbde87ed7a478fc9f67ce2d1b03e8e773790
pitten <pitten82@gmail.com>
2021-08-07 23:34:53 -0700

    1

---
+++ main_BLEAK.py
@@ -0,0 +1,58 @@
+"""
+Notifications
+-------------
+Example showing how to add notifications to a characteristic and handle the responses.
+Updated on 2019-07-03 by hbldh <henrik.blidh@gmail.com>
+"""
+
+import sys
+import logging
+import asyncio
+import platform
+
+from bleak import BleakClient
+from bleak import _logger as logger
+
+ADDRESS = ("F8:8A:5E:36:CB:34")  # <--- Change to your device's address here if you are using Windows or Linux
+CHARACTERISTIC_UUID_1 = "835ab4c0-51e4-11e3-a5bd-0002a5d5c51b"  # <--- Change to the characteristic you want to enable notifications from.
+CHARACTERISTIC_UUID_2 = "00002a1c-0000-1000-8000-00805f9b34fb"  # <--- Change to the characteristic you want to enable notifications from.
+
+
+
+if len(sys.argv) == 3:
+    ADDRESS = sys.argv[1]
+    CHARACTERISTIC_UUID_1 = sys.argv[2]
+
+
+def notification_handler(sender, data):
+    """Simple notification handler which prints the data received."""
+    print("{0}: {1}".format(sender, data))
+
+
+async def run(address, debug=True):
+    if debug:
+        import sys
+
+        l = logging.getLogger("asyncio")
+        l.setLevel(logging.DEBUG)
+        h = logging.StreamHandler(sys.stdout)
+        h.setLevel(logging.DEBUG)
+        l.addHandler(h)
+        logger.addHandler(h)
+
+    async with BleakClient(address) as client:
+        logger.info(f"Connected: {client.is_connected}")
+
+        await client.start_notify(CHARACTERISTIC_UUID_1, notification_handler)
+        await client.start_notify(CHARACTERISTIC_UUID_2, notification_handler)
+        await asyncio.sleep(60.0)
+        await client.stop_notify(CHARACTERISTIC_UUID_1)
+        await client.stop_notify(CHARACTERISTIC_UUID_2)
+
+if __name__ == "__main__":
+    import os
+
+    os.environ["PYTHONASYNCIODEBUG"] = str(1)
+    loop = asyncio.get_event_loop()
+    # loop.set_debug(True)
+    loop.run_until_complete(run(ADDRESS, True))
--- main_notification.py
+++ main_notification.py
@@ -1,6 +1,10 @@
 from time import sleep
 import pygatt
+import logging
 from influxdb import InfluxDBClient
+
+logging.basicConfig()
+logging.getLogger('pygatt').setLevel(logging.DEBUG)
 
 sensor_mac = "F8:8A:5E:36:CB:34"
 adapter = pygatt.GATTToolBackend(hci_device='hci0')
@@ -9,6 +13,29 @@ client = InfluxDBClient(host='192.168.128.92',
                         database="KP_lab_data")
 
 
+# Celsius to Fahrenheit conversion
+def fahrenheit(celcius):
+    return int(round(celcius * (9/5.0) + 32))
+
+
+# Process and save the realtime data
+def handle_notification(handle, value):
+    """
+    handle -- integer, characteristic read handle the data was received on
+    value -- bytearray, the data returned in the notification
+    """
+    print(handle, value)
+
+    # if handle == 0x000b:
+    #     pressure = int(int.from_bytes(value, "big"))
+    #     print(f"pressure: ", pressure)
+
+    # if handle == 0x0012:
+    #     temperature = value
+    #     print(f"temperature: ", temperature)
+
+    # print(handle)
+    # print(value)
 
 
 try:
@@ -21,39 +48,79 @@ try:
     except:
         print("couldn't connect, retrying...")
         device = adapter.connect(sensor_mac, timeout=10)
+        print("connected...")
+    sleep(5)
 
-    while True:
+    # Enable notifications
+    notification_enable = bytearray([0x01,0x00])
+    measurement_interval = bytearray([0x05,0x00])
 
-        print("reading")
-        #read specific characteristics
-        pressure_hex = device.char_read_handle("0x000b")
-        battery_hex = device.char_read_handle("0x0018")
-        temperature_hex = device.char_read_handle("0x0012")
+    # device.char_write_handle(21, measurement_interval)
+    # sleep(5)
+    # device.char_write_handle(0xd, notification_enable) # pressure
+    # sleep(5)
+    # device.char_write_handle(0x13, notification_enable) # temperature
+    
+    device.subscribe("835ab4c0-51e4-11e3-a5bd-0002a5d5c51b", 
+                            callback=handle_notification)
+    
+    # try:
+    #     device.subscribe("835ab4c0-51e4-11e3-a5bd-0002a5d5c51b", 
+    #                     callback=handle_notification)
+    # except Exception as e:
+    #     try:
+    #         sleep(5)
+    #         device.subscribe("835ab4c0-51e4-11e3-a5bd-0002a5d5c51b",
+    #                         callback=handle_notification)
+    #     except:
+    #         pass
 
-        #convert hex data
-        pressure = int(int.from_bytes(pressure_hex, "big"))
-        battery = int(int.from_bytes(battery_hex, "big"))
-        temperature = (int.from_bytes(temperature_hex[2:3], "big"))/10
 
-        if pressure > 65000:
-            pressure = pressure -65536
+    # device.subscribe("00002a1c-0000-1000-8000-00805f9b34fb", 
+    #                 callback=handle_notification, 
+    #                 wait_for_response=False)
+    # try:
+    #     device.subscribe("835ab4c0-51e4-11e3-a5bd-0002a5d5c51b", callback=handle_notification)
+    #     # device.subscribe("00002a1c-0000-1000-8000-00805f9b34fb", callback=handle_notification)
+    # except Exception as e:
+    #         try:
+    #             device.subscribe("835ab4c0-51e4-11e3-a5bd-0002a5d5c51b", callback=handle_notification)
+    #             # device.subscribe("00002a1c-0000-1000-8000-00805f9b34fb", callback=handle_notification)
+    #         except:
+    #             pass
 
-        json_body = [
-            {"measurement": "pressures",
-            "fields": {"P1": pressure, "batt1": battery, "temp1": temperature}}
-            ]
-        
+    # pressure_hex = device.char_read_handle("0x000b")
+    # battery_hex = device.char_read_handle("0x0018")
+    # temperature_hex = device.char_read_handle("0x0012")
 
-        client.write_points(json_body)
+    #convert hex data
+    # pressure = int(int.from_bytes(pressure_hex, "big"))
+    # battery = int(int.from_bytes(battery_hex, "big"))
+    # temperature = (int.from_bytes(temperature_hex[2:3], "big"))/10
 
-        #print(hexlify(data))
-        print(f"pressure: {pressure} - {hexlify(pressure_hex)}")
-        print(f"battery: {battery} - {hexlify(battery_hex)}")
-        print(f"temp: {temperature} - {(temperature_hex)}")
-        
-        #input("press any key")
-        sleep(10)
+    # if pressure > 65000:
+    #     pressure = pressure -65536
+
+    # json_body = [
+    #     {"measurement": "pressures",
+    #     "fields": {"P1": pressure, "batt1": battery, "temp1": temperature}}
+    #     ]
+    
+
+    # client.write_points(json_body)
+
+    # #print(hexlify(data))
+    # print(f"pressure: {pressure} - {hexlify(pressure_hex)}")
+    # print(f"battery: {battery} - {hexlify(battery_hex)}")
+    # print(f"temp: {temperature} - {(temperature_hex)}")
+    
+    #input("press any key")
+    # sleep(10)
+
+    input("Enter any key to quit....")
+    print("\n")
 
 finally:
-    device.disconnect()
+    print("stopping..")
     adapter.stop()
+    print("stopped")
